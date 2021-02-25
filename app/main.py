85f7b79cfd9c9b6785bb7f2c8d61bd33e671534c
masudbarki <61346065+masudbaki@users.noreply.github.com>
2021-02-25 20:41:36 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -26,6 +26,7 @@ def predict():
   #urllib.request.urlretrieve(tokenizer_url,"tokenizer.pickle")
   #myfile = requests.get("https://drive.google.com/uc?export=download&id=1-5PXkN3D8uXTvtd6rL-lDI1pO3CvWdfv")
   ##open('tokenizer.pickle', 'wb').write(myfile.content)
+  f_result_string = []
   filename = "/app/tokenizer.pickle"
   
   with zipfile.ZipFile("tokenizer.zip", 'r') as zip_ref:
@@ -51,8 +52,83 @@ def predict():
   interpreter.invoke()
   output_data = interpreter.get_tensor(output_details[0]['index'])
   print(output_data[0][0])
-  return message
-    
+  if round(output_data[0][0]) == 0:
+    f_result_string.append("F")
+  else:
+    f_result_string.append("T")
+  print(f_result_string)
   
+  
+  #second model
+  model_url = "https://drive.google.com/uc?export=download&id=1-6J_wixl_I0A2pHJyGD2C9dq8c_4xCA2"
+  urllib.request.urlretrieve(model_url,"IE_model.tflite")
+  interpreter = tf.lite.Interpreter(model_path="IE_model.tflite")
+  interpreter.allocate_tensors()
+  print("model Loaded")
+    # Get input and output tensors.
+  input_details = interpreter.get_input_details()
+  output_details = interpreter.get_output_details()
+    # Test the model on random input data.
+  input_shape = input_details[0]['shape']
+  input_data = np.array(processed_string, dtype=np.float32)
+  interpreter.set_tensor(input_details[0]['index'], input_data)
+  interpreter.invoke()
+  output_data = interpreter.get_tensor(output_details[0]['index'])
+  print(output_data[0][0])
+  if round(output_data[0][0]) == 0:
+    f_result_string.append("I")
+  else:
+    f_result_string.append("E")
+  print(f_result_string)
+  
+  
+  #3rd model
+  
+  
+  model_url = "https://drive.google.com/uc?export=download&id=1-BZUnV6zavnvDfVEGhKHl9PeUdqFykLY"
+  urllib.request.urlretrieve(model_url,"JP_model.tflite")
+  interpreter = tf.lite.Interpreter(model_path="JP_model.tflite")
+  interpreter.allocate_tensors()
+  print("model Loaded")
+    # Get input and output tensors.
+  input_details = interpreter.get_input_details()
+  output_details = interpreter.get_output_details()
+    # Test the model on random input data.
+  input_shape = input_details[0]['shape']
+  input_data = np.array(processed_string, dtype=np.float32)
+  interpreter.set_tensor(input_details[0]['index'], input_data)
+  interpreter.invoke()
+  output_data = interpreter.get_tensor(output_details[0]['index'])
+  print(output_data[0][0])
+  if round(output_data[0][0]) == 0:
+    f_result_string.append("J")
+  else:
+    f_result_string.append("P")
+  print(f_result_string)
+  
+  #4th model
+  
+   model_url = "https://drive.google.com/uc?export=download&id=1-Bdta2WiKax-3RZ0EqHAx42RD7JfBq40"
+  urllib.request.urlretrieve(model_url,"NS_model.tflite")
+  interpreter = tf.lite.Interpreter(model_path="NS_model.tflite")
+  interpreter.allocate_tensors()
+  print("model Loaded")
+    # Get input and output tensors.
+  input_details = interpreter.get_input_details()
+  output_details = interpreter.get_output_details()
+    # Test the model on random input data.
+  input_shape = input_details[0]['shape']
+  input_data = np.array(processed_string, dtype=np.float32)
+  interpreter.set_tensor(input_details[0]['index'], input_data)
+  interpreter.invoke()
+  output_data = interpreter.get_tensor(output_details[0]['index'])
+  print(output_data[0][0])
+  if round(output_data[0][0]) == 0:
+    f_result_string.append("N")
+  else:
+    f_result_string.append("S")
+  print(f_result_string)
+  
+  return f'{f_result_string[0]} {f_result_string[1]} {f_result_string[2]} {f_result_string[3]}'
 
 
